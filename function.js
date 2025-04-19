5be3270e71e0a9adb4f281f57eac56ec81725414
aayushjha658 <aayushjha658@gmail.com>
2025-04-19 09:53:15 +0530

    Javascript uploaded

---
+++ function.js
@@ -0,0 +1,63 @@
+
+
+//1)
+// var myName = "Aayush Jha";
+// myName = "Aayush B Jha";
+// console.log(myName);
+
+// pi=3.142 * r * r;
+
+//2)
+//  const myName = "Aayush Jha";
+// console.log(myName);
+
+// 3)
+// let age = 30;
+// let message = `I am ${age} years old.`;
+// console.log(message);
+
+// 4)
+//  3 students at a same time wants to find the sum of two numbers 
+// 1st student
+// var a = 15,
+// b = 15;
+// var sum2 = a + b;
+// console.log(sum2);
+
+
+// 5)
+//  function greet(){
+
+//     console.log("Hello, World!");
+// }
+// greet();
+
+// 6)
+//  function sum(a, b){
+//     return a * b;
+// }
+
+// console.log(sum(5, 5));
+// console.log(sum(15, 50));
+// console.log(sum(100, 200));
+
+// 7)
+//  function greet(name){
+//     console.log("Hello, " + name + ",Welcome to Js Course");
+// }
+
+// var result = function sum(a, b) {
+//     console.log(a + b);
+// };
+
+// result(10, 15);
+
+// 8)
+// function sum(a, b){
+//     return a + b;
+//     // console.log("This will not be executed");
+//     console.log("Hello world");
+// }
+
+// var result = sum(5, 5);
+// console.log(result);
---
+++ loop.js
@@ -0,0 +1,36 @@
+
+//console.log("5 * $(num) = $(5 * num)");
+
+// do while loops
+
+
+// var num =1;
+// while (num <= 10) {
+//     console.log("5 * $(num) = $(5 * num)");
+//     num++;
+// }
+
+// let userInput;
+// let positiveNumber;
+// do{
+//     userInput = prompt("Enter a positive number");
+//     positiveNumber = parseFloat(userInput);
+// } while (isNaN(positiveNumber) || positiveNumber <= 0);
+// console.log("You entered a valid positive number:", positiveNumber);
+
+// var num = 1;
+// while (num <= 10){
+//     console.log("5 * " + num + " = " + (5 * num));
+//     num++;
+// }
+
+//! Homework
+// javascript program to print table for given number (8,9,12,15) using for loop?
+
+// var num = 1;
+// while (num <= 10){
+//     console.log("8 * " + num + " = " + (8 * num));
+//     num++;
+// }
+
+// ? if a year is divisible by 4 and not divisible by 100, or
